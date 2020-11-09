7e6cd0c5c0dd8ff62b67deb394c5bcf8d3805fc7
Arkadiy Kleymenov <72223169+l-arkadiy-l@users.noreply.github.com>
2020-11-09 09:15:40 +0300

    Update ex5.py

--- lesson 4/ex5.py
+++ lesson 4/ex5.py
@@ -3,8 +3,9 @@ b = int(input('Введите ширину прямоугольника: '))
 c = int(input('Введите сторону квадрата: '))
 counter = 0
 n = a * b
-while n >= c ** 2:
-    n -= c ** 2
-    counter += 1
+if a > c and b > c:
+    while n >= c ** 2:
+        n -= c ** 2
+        counter += 1
 print('В прямоугольнике {}х{} можно разместить {} квадратов со стороной {}'.format(a, b, counter, c))
 print('Площадь незанятой части {}'.format(n))
