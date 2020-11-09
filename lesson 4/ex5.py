249560ebf724ddd11ef5845f5fdcbe879d0bf017
Arkadiy Kleymenov <72223169+l-arkadiy-l@users.noreply.github.com>
2020-11-09 09:15:52 +0300

    Update ex5.py

--- lesson 4/ex5.py
+++ lesson 4/ex5.py
@@ -3,7 +3,7 @@ b = int(input('Введите ширину прямоугольника: '))
 c = int(input('Введите сторону квадрата: '))
 counter = 0
 n = a * b
-if a > c and b > c:
+if a >= c and b >= c:
     while n >= c ** 2:
         n -= c ** 2
         counter += 1
