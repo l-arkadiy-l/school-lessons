6cc67c957df7d44d57f2ed214e0fd84157e26a5a
Arkadiy Kleymenov <72223169+l-arkadiy-l@users.noreply.github.com>
2020-11-29 15:49:45 +0300

    Create ex1.py

---
+++ lesson 5/ex1.py
@@ -0,0 +1,4 @@
+number = int(input())
+print('{} >= 0 -- {}'.format(number, number >= 0))
+print('{} % 2 == 0 -- {}'.format(number, number % 2 == 0))
+print('{} == 7 -- {}'.format(number, number == 7))
